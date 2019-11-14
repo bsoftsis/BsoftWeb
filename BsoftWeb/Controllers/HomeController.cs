c673388afa46e8fe7dd6462ea96fcda90e1f605e
Win 10 <Win 10@WIN10-PC>
2019-11-14 02:58:59 -0300

    Modificacion de Home Controller Action Contact, y la vista  Contac- Se hizo funsioran formulario de contacto enviando correo a la cuenta bsoftsis@gmail.com

--- BsoftWeb/Controllers/HomeController.cs
+++ BsoftWeb/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,5 +27,44 @@ namespace BsoftWeb.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(String razon, String email, String mensaje)
+        {
+
+            /* con este codigo se puede enviar correo desde el formulario de la web
+             hacia la casilla de bsoftsis*/
+            try
+            {
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress(email);
+                correo.To.Add("bsoftsis@gmail.com");
+                correo.Subject = "Mensaje de " + razon;
+                correo.Body = mensaje;
+                correo.IsBodyHtml = true;
+                correo.Priority = MailPriority.Normal;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+                string sCuentaCorreo = "bsoftsis@gmail.com";
+                string sPassWordCorreo = "Admrec2019";
+                smtp.Credentials = new System.Net.NetworkCredential(sCuentaCorreo, sPassWordCorreo);
+                smtp.Send(correo);
+
+                ViewBag.Message = "Gracias por Escribirnos " + razon + "!!!!";
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Error al Enviar mensaje: " + ex.Message;
+            }
+
+
+            return View();
+        }
+
     }
 }
