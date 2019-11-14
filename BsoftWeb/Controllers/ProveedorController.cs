c6557c6b0f6af800aa6ef221c844066bb20cdf37
Win 10 <Win 10@WIN10-PC>
2019-11-14 00:47:57 -0300

    Agregar archivos de proyecto.

---
+++ BsoftWeb/Controllers/ProveedorController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BsoftWeb;
+
+namespace BsoftWeb.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        private BsoftDBEntities db = new BsoftDBEntities();
+
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            var proveedor = db.Proveedor.Include(p => p.Localidad);
+            return View(proveedor.ToList());
+        }
+
+        // GET: Proveedor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedor/Create
+        public ActionResult Create()
+        {
+            ViewBag.idLocalidad = new SelectList(db.Localidad, "idLocalidad", "nombreLocalidad");
+            return View();
+        }
+
+        // POST: Proveedor/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProveedor,razonSocial,cuit,domicilio,telefono,celular,email,estado,fechaRegistro,idLocalidad")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proveedor.Add(proveedor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idLocalidad = new SelectList(db.Localidad, "idLocalidad", "nombreLocalidad", proveedor.idLocalidad);
+            return View(proveedor);
+        }
+
+        // GET: Proveedor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idLocalidad = new SelectList(db.Localidad, "idLocalidad", "nombreLocalidad", proveedor.idLocalidad);
+            return View(proveedor);
+        }
+
+        // POST: Proveedor/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idProveedor,razonSocial,cuit,domicilio,telefono,celular,email,estado,fechaRegistro,idLocalidad")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proveedor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idLocalidad = new SelectList(db.Localidad, "idLocalidad", "nombreLocalidad", proveedor.idLocalidad);
+            return View(proveedor);
+        }
+
+        // GET: Proveedor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            db.Proveedor.Remove(proveedor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BsoftWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BsoftWeb.Startup))]
+namespace BsoftWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
